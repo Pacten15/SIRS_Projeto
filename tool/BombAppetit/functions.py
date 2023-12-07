5bf85da8e6d92c773aa6aeadd42250e3abb219a2
Pacten15 <luismarques718@hotmail.com>
2023-12-07 07:13:47 -0500

    Secure Documents progress

--- tool/BombAppetit/functions.py
+++ tool/BombAppetit/functions.py
@@ -9,6 +9,21 @@ from Crypto.Random       import get_random_bytes
 from Crypto.Signature    import pkcs1_15
 from Crypto.Util.Padding import pad, unpad
 
+
+def read_random_bytes_or_encrypted_files(file_path):
+    with open(file_path, 'rb') as f:
+        data = f.read()
+    f.close()
+    return data
+
+def create_json_file(file_path, data):
+    with open(file_path, 'w') as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
+    f.close()
+
+
+
+
 def read_private_key(private_key_path):
     try:
         with open(private_key_path, 'rb') as file:
@@ -183,3 +198,34 @@ def check(infile_path, src_public_key_path, dst_private_key_path):
         exit(1)
 
     print("Document verified!")
+
+
+    def encrypt_json_section(raw_bytes, src_private_key, dst_public_key, section_bytes):
+        ''' Encrypts content using generated AES key, AES key will be encrypted
+        with dst_public_key for confidentiality, the contents will be hashed,
+        for integrity, and signed using src_private_key, for autheticity.'''
+
+
+        # Generate AES key and encrypt contents
+        gen_key = get_random_bytes(32) # 32 for AES-256
+        gen_cipher = AES.new(gen_key, AES.MODE_CBC)
+        ciphertext = gen_cipher.encrypt(pad(raw_content, AES.block_size))
+        encrypted_content = b64encode(ciphertext).decode('utf-8')
+
+        # Encrypt AES key and IV with public RSA key
+        rsa_cipher = PKCS1_v1_5.new(dst_public_key)
+        ciphertext = rsa_cipher.encrypt(gen_key+gen_cipher.iv)
+        encrypted_key = b64encode(ciphertext).decode('utf-8')
+
+        # Create contents digest and sign
+        hashed = SHA256.new(encrypted_content.encode('utf-8'))
+        signer = pkcs1_15.new(src_private_key)
+        ciphertext = signer.sign(hashed)
+        encrypted_hash = b64encode(ciphertext).decode('utf-8')
+
+        return {"encrypted_content": encrypted_content, "encrypted_key": encrypted_key, "encrypted_hash": encrypted_hash}
+
+        
+
+        
+        
