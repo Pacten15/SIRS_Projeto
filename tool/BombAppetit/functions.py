59fbb5d35ed76e53e8707e7ca4390c4c7a4b2ad5
Pacten15 <luismarques718@hotmail.com>
2023-12-09 09:25:25 -0500

    feat: Encryption and Dencryption of section

--- tool/BombAppetit/functions.py
+++ tool/BombAppetit/functions.py
@@ -9,21 +9,6 @@ from Crypto.Random       import get_random_bytes
 from Crypto.Signature    import pkcs1_15
 from Crypto.Util.Padding import pad, unpad
 
-
-def read_random_bytes_or_encrypted_files(file_path):
-    with open(file_path, 'rb') as f:
-        data = f.read()
-    f.close()
-    return data
-
-def create_json_file(file_path, data):
-    with open(file_path, 'w') as f:
-        json.dump(data, f, indent=4, ensure_ascii=False)
-    f.close()
-
-
-
-
 def read_private_key(private_key_path):
     try:
         with open(private_key_path, 'rb') as file:
@@ -120,7 +105,7 @@ def protect(infile_path, src_private_key_path, dst_public_key_path, outfile_path
     src_private_key = read_private_key(src_private_key_path)
     dst_public_key  = read_public_key(dst_public_key_path)
 
-    encrypted_document = encrypt(raw_content, src_private_key, dst_public_key)
+    encrypted_document = encrypt_json_section(raw_content, src_private_key, dst_public_key, 'mealVoucher')
     try:
         with open(outfile_path, 'w') as outfile:
             json.dump(encrypted_document, outfile, indent=3)
@@ -149,7 +134,7 @@ def unprotect(infile_path, src_public_key_path, dst_private_key_path, outfile_pa
         nonces = []
 
     try:
-        raw_content = decrypt(encrypted_document, src_public_key, dst_private_key, nonces)
+        raw_content = decrypt_json_section(encrypted_document, src_public_key, dst_private_key, nonces, 'mealVoucher')
     except (AssertionError, ValueError, KeyError):
         print("ERROR: Failed document decryption.", file=sys.stderr)
         exit(1)
@@ -200,32 +185,87 @@ def check(infile_path, src_public_key_path, dst_private_key_path):
     print("Document verified!")
 
 
-    def encrypt_json_section(raw_bytes, src_private_key, dst_public_key, section_bytes):
-        ''' Encrypts content using generated AES key, AES key will be encrypted
-        with dst_public_key for confidentiality, the contents will be hashed,
-        for integrity, and signed using src_private_key, for autheticity.'''
+def encrypt_json_section(raw_bytes, src_private_key, dst_public_key, section):
+    ''' Encrypts content using generated AES key, AES key will be encrypted
+    with dst_public_key for confidentiality, the contents will be hashed,
+    for integrity, and signed using src_private_key, for autheticity.'''
 
 
-        # Generate AES key and encrypt contents
-        gen_key = get_random_bytes(32) # 32 for AES-256
-        gen_cipher = AES.new(gen_key, AES.MODE_CBC)
-        ciphertext = gen_cipher.encrypt(pad(raw_content, AES.block_size))
-        encrypted_content = b64encode(ciphertext).decode('utf-8')
+    decrypted_Documents = json.loads(raw_bytes)
 
-        # Encrypt AES key and IV with public RSA key
-        rsa_cipher = PKCS1_v1_5.new(dst_public_key)
-        ciphertext = rsa_cipher.encrypt(gen_key+gen_cipher.iv)
-        encrypted_key = b64encode(ciphertext).decode('utf-8')
+    section_content = decrypted_Documents['restaurantInfo'][section]
 
-        # Create contents digest and sign
-        hashed = SHA256.new(encrypted_content.encode('utf-8'))
-        signer = pkcs1_15.new(src_private_key)
-        ciphertext = signer.sign(hashed)
-        encrypted_hash = b64encode(ciphertext).decode('utf-8')
+    section_bytes = json.dumps(section_content).encode('utf-8')
 
-        return {"encrypted_content": encrypted_content, "encrypted_key": encrypted_key, "encrypted_hash": encrypted_hash}
+    # Generate AES key and encrypt contents
+    gen_key = get_random_bytes(32) # 32 for AES-256
+    gen_cipher = AES.new(gen_key, AES.MODE_CBC)
+    ciphertext = gen_cipher.encrypt(pad(section_bytes, AES.block_size))
+    section_content = b64encode(ciphertext).decode('utf-8')
+    decrypted_Documents['restaurantInfo'][section] = section_content
+    encrypted_content = b64encode(json.dumps(decrypted_Documents).encode('utf-8')).decode('utf-8')
 
-        
+    # Encrypt AES key and IV with public RSA key
+    rsa_cipher = PKCS1_v1_5.new(dst_public_key)
+    ciphertext = rsa_cipher.encrypt(gen_key+gen_cipher.iv)
+    encrypted_key = b64encode(ciphertext).decode('utf-8')
+    # Create contents digest and sign
+    hashed = SHA256.new(encrypted_content.encode('utf-8'))
+    signer = pkcs1_15.new(src_private_key)
+    ciphertext = signer.sign(hashed)
+    encrypted_hash = b64encode(ciphertext).decode('utf-8')
 
+    
+    return {"encrypted_content": encrypted_content, "encrypted_key": encrypted_key, "encrypted_hash": encrypted_hash}
+
+
+def decrypt_json_section(encrypted_document, src_public_key, dst_private_key, nonces, section):
+    ''' Decrypts encrypted_document using AES, AES key is found by decrypting
+    with dst_private_key, the signature is checked using src_public_key,
+    and the decrypted contents are returned directly.'''
+
+    encrypted_content = encrypted_document['encrypted_content']
+    encrypted_key = encrypted_document['encrypted_key']
+    encrypted_hash = encrypted_document['encrypted_hash']
+
+    # Decrypt AES key and IV with private RSA key
+    sentinel = get_random_bytes(32 + 16)
+    rsa_cipher = PKCS1_v1_5.new(dst_private_key)
+    ciphertext = b64decode(encrypted_key.encode())
+    gen_key_iv = rsa_cipher.decrypt(ciphertext, sentinel, expected_pt_len=32 + 16)
+    assert gen_key_iv != sentinel
+    gen_key = gen_key_iv[:32]
+    gen_iv  = gen_key_iv[32:]
+
+     # Test raw_content with the signed digest
+    hashed = SHA256.new(encrypted_content.encode('utf-8'))
+    signer = pkcs1_15.new(src_public_key)
+    signature = b64decode(encrypted_hash.encode())
+    signer.verify(hashed, signature)
+    assert signature.hex() not in nonces
+    nonces.append(signature.hex())
+
+
+    encrypted_content_bytes = b64decode(encrypted_content.encode('utf-8'))
+
+    decrypted_Documents = json.loads(encrypted_content_bytes.decode('utf-8'))
+
+    section_content = decrypted_Documents['restaurantInfo'][section]
+
+    section_bytes = b64decode(section_content.encode('utf-8'))
+
+    # Decrypt content with AES key and IV
+    ciphertext = section_bytes
+    gen_cipher = AES.new(gen_key, AES.MODE_CBC, gen_iv)
+    raw_content = unpad(gen_cipher.decrypt(ciphertext), AES.block_size)
+
+    decrypted_Documents['restaurantInfo'][section] = json.loads(raw_content)
+
+    print(decrypted_Documents)
+
+    decrypted_Documents_bytes = json.dumps(decrypted_Documents, indent=2, ensure_ascii=False).encode('utf-8')
+
+    return decrypted_Documents_bytes
+    
         
         
