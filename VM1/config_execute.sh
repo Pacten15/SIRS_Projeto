66d992f867c1a75cf8da5c7d82501c29611e2bc2
Pacten15 <luismarques718@hotmail.com>
2023-12-21 05:51:45 -0500

    VM2 config

--- VM1/config_execute.sh
+++ VM1/config_execute.sh
@@ -2,9 +2,15 @@
 
 sudo cp interfaces /etc/network/interfaces
 
+sudo systemctl enable postgresql
+
+sudo systemctl start postgresql
+
 sudo cp postgresql.conf /etc/postgresql/16/main/postgresql.conf
 
 sudo -u postgres psql -c "CREATE USER sirs_dbadmin WITH PASSWORD 'sirs_dbpassword';"
 
 sudo -u postgres psql -c "CREATE DATABASE sirs_bombappetit WITH OWNER sirs_dbadmin ENCODING='UTF8' TEMPLATE=template0;"
+
+sudo systemctl enable ssh
 
