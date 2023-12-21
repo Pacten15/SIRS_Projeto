54a3ae3f361c440922b78d1233f16d17f6d38c45
Pacten15 <luismarques718@hotmail.com>
2023-12-21 05:19:08 -0500

    VM1 config

---
+++ VM1/config_execute.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo cp interfaces /etc/network/interfaces
+
+sudo cp postgresql.conf /etc/postgresql/16/main/postgresql.conf
+
+sudo -u postgres psql -c "CREATE USER sirs_dbadmin WITH PASSWORD 'sirs_dbpassword';"
+
+sudo -u postgres psql -c "CREATE DATABASE sirs_bombappetit WITH OWNER sirs_dbadmin ENCODING='UTF8' TEMPLATE=template0;"
+
